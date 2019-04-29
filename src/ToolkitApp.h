043fb946cb77647f7c1fa5a7ab40d40edd868c00
Felix Richter <judge@felixrichter.tech>
2019-04-29 16:49:24 +0200

    added barebone qt sceleton

---
+++ src/ToolkitApp.h
@@ -0,0 +1,5 @@
+#ifndef ToolkitApp_h_INCLUDED
+#define ToolkitApp_h_INCLUDED
+
+#endif
+
---
+++ src/main.cc
@@ -0,0 +1,11 @@
+#include <QApplication>
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	return app.exec();
+}
