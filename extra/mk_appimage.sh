cff294eafdf277a928dd3edfe6164d8305ba1ea4
ju6ge <judge@felixrichter.tech>
2020-08-27 14:26:52 +0200

    appimage adding required qt geometry- and scene-loader plugins

--- extra/mk_appimage.sh
+++ extra/mk_appimage.sh
@@ -13,16 +13,30 @@ done
 # install application to appdir
 make install DESTDIR=rbdl-toolkit.AppDir
 
+if (( $? )); then
+	exit 1
+fi
+
 # populate appdir with need libs
 linuxdeploy --appdir=rbdl-toolkit.AppDir -e rbdl-toolkit -i ../extra/rbdl-toolkit.png --create-desktop-file
+
+if (( $? )); then
+	exit 1
+fi
 
 # pull correct qt dependencies into appdir
 if [ "$customqt" == true ]
 then
-	QMAKE=${CUSTOM_QT_PATH}/bin/qmake linuxdeploy-qt --appdir=rbdl-toolkit.AppDir
+	QMAKE=${CUSTOM_QT_PATH}/bin/qmake linuxdeploy-qt --appdir=rbdl-toolkit.AppDir -p libdefaultgeometryloader.so -p 3dquickrender
 else
 	linuxdeploy-qt --appdir=rbdl-toolkit.AppDir
 fi
 
+if (( $? )); then
+	exit 1
+fi
+
 #create AppImage
 appimagetool rbdl-toolkit.AppDir
+
+exit 0
