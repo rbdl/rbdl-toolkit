9fdfcbb73e9b1f178582a8c36e6b9a68fdbd236b
ju6ge <judge@felixrichter.tech>
2020-11-13 18:28:38 +0100

    find urdf meshes logic added

--- src/urdfmodel_wrapper.cc
+++ src/urdfmodel_wrapper.cc
@@ -1,4 +1,6 @@
 #include <fstream>
+#include <QFileInfo>
+#include <QDir>
 
 #include "urdfmodel_wrapper.h"
 #include "urdf_parser/urdf_parser.h"
@@ -43,11 +45,67 @@ void UrdfModelWrapper::load(QString model_file) {
 }
 
 ModelInfo UrdfModelWrapper::loadModelInfo() {
+	QMatrix4x4 orientation;
+	orientation.rotate(-90., 1., 0., 0.);
 	ModelInfo info = { 
-	                   QMatrix4x4(),
+	                   orientation,
 	                   1.
 	                 };
 	return info;
+}
+
+// this function tries to merge paths based on common point
+QString mergePaths(QString upper, QString lower) {
+	int i=1;
+	while (i < upper.size() && i < lower.size()) {
+		if (upper.endsWith(lower.left(i), Qt::CaseInsensitive)) {
+			QString merge = QString(upper).append(lower.right(lower.size()-i));
+			auto info = QFileInfo(merge);
+			if (info.exists()) {
+				return merge;
+			}
+		}
+		i++;
+	}
+	throw std::exception();
+}
+
+void findRelativeToRosPackageRoot(QString& path, QString model_file_path) {
+	QString backup_orig = QString(path);
+	path.replace("package://", "");
+	auto package_path = QFileInfo(model_file_path).absoluteDir();
+
+	// if we now find the file great, stop here
+	if (package_path.exists(path)) {
+		path = package_path.absoluteFilePath(path);
+		return;
+	}
+
+	// try to find the package root, by looking for the package xml
+	while ( !package_path.exists("package.xml") && !package_path.isRoot() ) {
+		package_path.cdUp();
+	}
+
+	// if we now find the file great, stop here
+	if (package_path.exists(path)) {
+		path = package_path.absoluteFilePath(path);
+		return;
+	}
+	// we did not find the file
+
+	// if we went to the root go back where we started from
+	// else we have found the Ros Package root and should look from there
+	if (package_path.isRoot()) {
+		package_path = QFileInfo(model_file_path).absoluteDir();
+	}
+
+	try {
+		path = mergePaths(package_path.canonicalPath(), path);
+	} catch (std::exception &e){
+		std::ostringstream errormsg;
+		errormsg << "No location found to load resource: " << backup_orig.toStdString() << " from! This model may be broken." << std::endl;
+		throw RigidBodyDynamics::Errors::RBDLFileParseError(errormsg.str());
+	}
 }
 
 std::vector<SegmentVisualInfo> UrdfModelWrapper::loadSegmentInfo() {
@@ -78,8 +136,12 @@ std::vector<SegmentVisualInfo> UrdfModelWrapper::loadSegmentInfo() {
 			auto rot = visual->origin.rotation;
 			QQuaternion mesh_rotation = QQuaternion(rot.x, rot.y, rot.z, rot.w);
 
-			auto color = visual->material->color;
-			QColor visual_color = QColor::fromRgbF(color.r, color.g, color.b, color.a);
+
+			QColor visual_color = QColor::fromRgbF(1., 1., 1., 1.);
+			if (visual->material != nullptr) {
+				auto color = visual->material->color;
+				visual_color = QColor::fromRgbF(color.r, color.g, color.b, color.a);
+			}
 
 			QString mesh_file;
 			QVector3D visual_center = QVector3D(0, 0, 0);
@@ -106,7 +168,11 @@ std::vector<SegmentVisualInfo> UrdfModelWrapper::loadSegmentInfo() {
 				}
 				case urdf::Geometry::MESH :{
 					urdf::Mesh* geo = (urdf::Mesh*)visual->geometry.get();
-					mesh_file = findFile(geo->filename, true);
+					QString filename = QString::fromStdString(geo->filename);
+					if (filename.startsWith("package://")) {
+						findRelativeToRosPackageRoot(filename, this->model_file);
+					}
+					mesh_file = findFile(filename, true);
 					visual_dimentions = QVector3D(geo->scale.x, geo->scale.y, geo->scale.z);
 					break;
 				}
