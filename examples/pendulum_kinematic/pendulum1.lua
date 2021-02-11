4340f327fde06a81f62c286a5bb7dbf101db12bb
Jonathan Lin <jf2lin@uwaterloo.ca>
2021-02-11 13:42:31 -0500

    wip

--- examples/pendulum_kinematic/pendulum1.lua
+++ examples/pendulum_kinematic/pendulum1.lua
@@ -1,16 +1,10 @@
-rod_length = 1
+rod_length = 5
 
 meshes = {
   rod1 = {
 	color = { 1, 0, 0},
 	mesh_center = {0, 0, -rod_length/2},
 	dimensions = { 0.2, 0.2, rod_length},
-	src = "unit_cube.obj"
-  },
-  rod2 = {
-	color = { 0, 1, 0},
-	mesh_center = {0, 0, -rod_length/2},
-	dimensions = { 0.1, 0.1, rod_length},
 	src = "unit_cube.obj"
   }
 }
@@ -27,19 +21,9 @@ model = {
 	  name = "segment1",
 	  parent = "ROOT",
 	  visuals = { meshes.rod1 },
-	  joint = {{ 0, 1, 0, 0, 0, 0 }},
+	  joint = {{ 0, 0, 0, 1, 0, 0 }},
 	  joint_frame = {
 		E = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}
-	  }
-	},
-	{
-	  name = "segment2",
-	  parent = "segment1",
-	  visuals = { meshes.rod2 },
-	  joint = {{ 0, 1, 0, 0, 0, 0 }},
-	  joint_frame = {
-		r = { 0., 0., -rod_length },
-		E = {{0, -1, 0}, {1, 0, 0}, {0, 0, 1}}
 	  }
 	}
   }
