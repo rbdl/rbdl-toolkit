0053f95ba1838e926377b6bbdafe4316be4ca42a
ju6ge <judge@felixrichter.tech>
2021-05-06 15:45:23 +0200

    working builtin shell Todo: fight mutlithreaded 🐉 -> fight no netcat 🐉 -> fight python undefined reference 🐉 -> add toolkit interface 🐉

--- plugins/PythonPlugin/PythonPlugin.cc
+++ plugins/PythonPlugin/PythonPlugin.cc
@@ -10,33 +10,33 @@
  * ToolkitSocketTerminal
  *=============================================================================*/
 
-ToolkitSocketTerminal::ToolkitSocketTerminal(const QString socket_addr, QWidget *parent)
-	: QTermWidget(false, parent)
- {
-	socket = new QLocalSocket(this);
-
-	connect(this, &ToolkitSocketTerminal::sendData, [this](const char *data, int size){
-		this->socket->write(data, size);
-	});
-
-	connect(socket, &QLocalSocket::readyRead, [this](){
-		QByteArray data = socket->readAll();
-		write(this->getPtySlaveFd(), data.data(), data.size());
-	});
-	connect(socket, &QLocalSocket::errorOccurred, this , &ToolkitSocketTerminal::atError);
-
-	this->startTerminalTeletype();
-
-	socket->connectToServer(socket_addr);
-}
-
-ToolkitSocketTerminal::~ToolkitSocketTerminal() {
-	socket->close();
-}
-
-void ToolkitSocketTerminal::atError(QLocalSocket::LocalSocketError err) {
-	std::cout << socket->errorString().toStdString() << std::endl;
-}
+//ToolkitSocketTerminal::ToolkitSocketTerminal(const QString socket_addr, QWidget *parent)
+//	: QTermWidget(false, parent)
+// {
+//	socket = new QLocalSocket(this);
+//
+//	connect(this, &ToolkitSocketTerminal::sendData, [this](const char *data, int size){
+//		this->socket->write(data, size);
+//	});
+//
+//	connect(socket, &QLocalSocket::readyRead, [this](){
+//		QByteArray data = socket->readAll();
+//		write(this->getPtySlaveFd(), data.data(), data.size());
+//	});
+//	connect(socket, &QLocalSocket::errorOccurred, this , &ToolkitSocketTerminal::atError);
+//
+//	this->startTerminalTeletype();
+//
+//	socket->connectToServer(socket_addr);
+//}
+//
+//ToolkitSocketTerminal::~ToolkitSocketTerminal() {
+//	socket->close();
+//}
+//
+//void ToolkitSocketTerminal::atError(QLocalSocket::LocalSocketError err) {
+//	std::cout << socket->errorString().toStdString() << std::endl;
+//}
 
 /*==============================================================================
  *PythonPlugin
@@ -82,19 +82,24 @@ void PythonPlugin::init(ToolkitApp* app) {
 	python_server = new PythonSocketServer(embedded_python);
 	python_server->startServer("toolkitpy.socket");
 
-//	console = new ToolkitSocketTerminal(python_server->fullServerName());
-//	QFont font = QApplication::font();
-//#ifdef Q_OS_MACOS
-//    font.setFamily(QStringLiteral("Monaco"));
-//#elif defined(Q_WS_QWS)
-//    font.setFamily(QStringLiteral("fixed"));
-//#else
-//    font.setFamily(QStringLiteral("Monospace"));
-//#endif
-//	font.setPointSize(11);
-//
-//    console->setTerminalFont(font);
-//
-//	parentApp->addView("ToolkitTerminal", qobject_cast<QWidget*>(console), Qt::BottomDockWidgetArea);
+	console = new QTermWidget(false);
+	QFont font = QApplication::font();
+#ifdef Q_OS_MACOS
+    font.setFamily(QStringLiteral("Monaco"));
+#elif defined(Q_WS_QWS)
+    font.setFamily(QStringLiteral("fixed"));
+#else
+    font.setFamily(QStringLiteral("Monospace"));
+#endif
+	font.setPointSize(11);
+
+    console->setTerminalFont(font);
+	console->setShellProgram(QString("nc"));
+	QStringList args;
+	args << QString("-U") << python_server->fullServerName();
+	console->setArgs(args);
+	console->startShellProgram();
+
+	parentApp->addView("ToolkitTerminal", qobject_cast<QWidget*>(console), Qt::BottomDockWidgetArea);
 }
 
