4fd4e001c9aee22b118f34e73603ccbee538c2f3
ju6ge <judge@felixrichter.tech>
2021-07-09 18:55:04 +0200

    better windows compiler detection

--- include/toolkit_config.h.cmake
+++ include/toolkit_config.h.cmake
@@ -19,12 +19,13 @@
 //
 // On Linux, set the visibility accordingly. If C++ symbol visibility
 // is handled by the compiler, see: http://gcc.gnu.org/wiki/Visibility
-# if defined _WIN32 || defined __CYGWIN__
+# if defined WINDOWS_BUILD || defined __CYGWIN__
 // On Microsoft Windows, use dllimport and dllexport to tag symbols.
 #  pragma warning(disable:4251) /*no DLL interface for type of member of exported class*/
 #  pragma warning(disable:4275) /*no DLL interface for base class of exported class*/
 //#  define TOOLKIT_DLLIMPORT __declspec(dllimport)
 //#  define TOOLKIT_DLLEXPORT __declspec(dllexport)
+#  include <QWidget>
 #  define TOOLKIT_DLLIMPORT Q_DECL_IMPORT
 #  define TOOLKIT_DLLEXPORT Q_DECL_EXPORT
 #  define TOOLKIT_DLLLOCAL
