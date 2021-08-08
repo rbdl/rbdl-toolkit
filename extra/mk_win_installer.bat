bb7e3cb018de0c9bca55e87576d9590f7227c576
ju6ge <judge@felixrichter.tech>
2021-08-08 16:24:56 +0200

    almost working build missing some opengl libraries on buildsystem probably

--- extra/mk_win_installer.bat
+++ extra/mk_win_installer.bat
@@ -1,8 +1,1 @@
-make install DESTDIR=RBDLToolkit_Deploy
-
-${WINDEPLOYQT_EXECUTABLE} -3dcore -3drenderer -3dinput -3danimation -3dextras -3dquick -winextras "RBDLToolkit_Deploy\Program Files (x86)\RBDL-Toolkit\bin\rbdl-toolkit.exe"
-copy "${RBDL_LIBRARY}" ".\RBDLToolkit_Deploy\Program Files (x86)\RBDL-Toolkit\bin\"
-copy "${RBDL_LUAMODEL_LIBRARY}" ".\RBDLToolkit_Deploy\Program Files (x86)\RBDL-Toolkit\bin\"
-copy "${RBDL_URDFREADER_LIBRARY}" ".\RBDLToolkit_Deploy\Program Files (x86)\RBDL-Toolkit\bin\"
-copy "${CUSTOM_QT_PATH}/bin/Qt53DLogic.dll" ".\RBDLToolkit_Deploy\Program Files (x86)\RBDL-Toolkit\bin\"
-cd %builddir%
+ ${WINDEPLOYQT_EXECUTABLE} -3dcore -3drenderer -3dinput -3danimation -3dextras -3dquick --ignore-library-errors %1
