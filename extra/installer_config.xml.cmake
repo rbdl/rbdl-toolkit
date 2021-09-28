60f3c580c97ff26608bab6c28e12f22f9f8cbd7b
ju6ge <judge@felixrichter.tech>
2021-09-28 18:10:23 +0200

    added more structure to package directory

---
+++ extra/installer_config.xml.cmake
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Installer>
+    <Name>RBDL-Toolkit</Name>
+    <Version>${TOOLKIT_VERSION}</Version>
+    <Title>RBDL-Toolkit Installer</Title>
+    <Publisher>ORB</Publisher>
+    <TargetDir>@ApplicationsDir@</TargetDir>
+</Installer>
