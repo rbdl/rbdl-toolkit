81c23842cc9a6d779108acc19bb3b4e016ac0b69
ju6ge <judge@felixrichter.tech>
2021-09-28 14:38:01 +0200

    wip installer creator script

--- cmake/FindIFW.cmake
+++ cmake/FindIFW.cmake
@@ -12,8 +12,18 @@ IF(IFW_INSTALL_PATH)
 
   IF( IFW_PATH )
     SET(IFW_FOUND TRUE)
+    FIND_PROGRAM( IFW_BINARYCREATOR bin/binarycreator.exe
+      PATHS
+      IFW_PATH
+      NO_DEFAULT_PATH
+    )
+    FIND_PROGRAM( IFW_BINARYCREATOR bin/archivegen.exe
+      PATHS
+      IFW_PATH
+      NO_DEFAULT_PATH
+    )
   ELSE( IFW_PATH )
-
+    MESSAGE(WARNING "IFW_PATH is set, but could not find required files. Please provide the correct path to the Qt Installer Framework!")
   ENDIF( IFW_PATH )
 
 ELSE(IFW_INSTALL_PATH)
