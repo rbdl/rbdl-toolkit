2399634ea6fe7fcbdd5ee2cdb613b0c02a26fa41
ju6ge <judge@felixrichter.tech>
2021-09-29 23:50:50 +0200

    create installer test

---
+++ extra/installer_package.xml.cmake
@@ -0,0 +1,18 @@
+<?xml version="1.0"?>
+<Package>
+    <DisplayName>RBDL-Toolkit</DisplayName>
+    <Description>Rigid Body Dynamics Toolkit</Description>
+    <Version>${TOOLKIT_VERSION}</Version>
+    <ReleaseDate>${TOOLKIT_BUILD_DATE}</ReleaseDate>
+    <Name>orb.rbdl-toolkit</Name>
+    <Virtual>false</Virtual>
+    <Licenses>
+        <License name="License Agreement" file="license.txt" />
+    </Licenses>
+    <SortingPriority>123</SortingPriority>
+    <UpdateText>This changed compared to the last release</UpdateText>
+    <Default>false</Default>
+    <ForcedInstallation>false</ForcedInstallation>
+    <ForcedUpdate>false</ForcedUpdate>
+    <Essential>false</Essential>
+</Package>
