1d99d23793297905254bdcf5b91ecb4b6571d3ef
ju6ge <judge@felixrichter.tech>
2021-09-29 23:58:20 +0200

    fix package.xml template

--- extra/installer_package.xml.cmake
+++ extra/installer_package.xml.cmake
@@ -5,13 +5,12 @@
     <Version>${TOOLKIT_VERSION}</Version>
     <ReleaseDate>${TOOLKIT_BUILD_DATE}</ReleaseDate>
     <Name>orb.rbdl-toolkit</Name>
-    <Virtual>false</Virtual>
     <Licenses>
         <License name="License Agreement" file="license.txt" />
     </Licenses>
     <SortingPriority>123</SortingPriority>
     <UpdateText>This changed compared to the last release</UpdateText>
-    <Default>false</Default>
+    <Default>true</Default>
     <ForcedInstallation>false</ForcedInstallation>
     <ForcedUpdate>false</ForcedUpdate>
     <Essential>false</Essential>
